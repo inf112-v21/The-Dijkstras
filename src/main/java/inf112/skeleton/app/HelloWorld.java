edc27af6b32affcecc32b86bf41e80e6348ae5ce
Aleksander <alexglo@outlook.com>
2021-02-08 16:37:12 +0100

    Map is displayable now. (Finished "showing the prior created board" part of the tutorial)

--- src/main/java/inf112/skeleton/app/HelloWorld.java
+++ src/main/java/inf112/skeleton/app/HelloWorld.java
@@ -4,18 +4,47 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL30;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import com.badlogic.gdx.maps.MapProperties;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileSets;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
 
 public class HelloWorld implements ApplicationListener {
     private SpriteBatch batch;
     private BitmapFont font;
+
+    public TiledMap map;
+    private TiledMapTileLayer Board;
+    private TiledMapTileLayer Player;
+    private TiledMapTileLayer Hole;
+    private TiledMapTileLayer Flag;
+
+    public OrthoCachedTiledMapRenderer orthoRenderer;
+    public OrthographicCamera camera;
+
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         font = new BitmapFont();
         font.setColor(Color.RED);
+        TiledMap map = new TmxMapLoader().load("assets/exampleMap.tmx");
+        TiledMapTileLayer Board = (TiledMapTileLayer) map.getLayers().get("Board");
+        TiledMapTileLayer Flag = (TiledMapTileLayer) map.getLayers().get("Flag");
+        TiledMapTileLayer Hole = (TiledMapTileLayer) map.getLayers().get("Hole");
+        TiledMapTileLayer Player = (TiledMapTileLayer) map.getLayers().get("Player");
+        camera = new OrthographicCamera(500, 500);
+        camera.setToOrtho(false, 5, 5);
+        camera.position.set(2.5f, 2.5f,0f);
+        camera.update();
+        orthoRenderer = new OrthoCachedTiledMapRenderer(map, (1/300f));
+
     }
 
     @Override
@@ -30,8 +59,11 @@ public class HelloWorld implements ApplicationListener {
         Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
 
         batch.begin();
-        font.draw(batch, "Hello World", 200, 200);
+        font.draw(batch, "Robo Rally", 200, 200);
+        orthoRenderer.setView(camera);
+        orthoRenderer.render();
         batch.end();
+
     }
 
     @Override
