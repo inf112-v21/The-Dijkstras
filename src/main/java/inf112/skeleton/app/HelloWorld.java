9ba18e73aff29a9650f30945f8a3ca78339942fc
anas <anasalmelhem17@gmail.com>
2021-02-08 21:43:47 +0100

    Tutorial in progress

--- src/main/java/inf112/skeleton/app/HelloWorld.java
+++ src/main/java/inf112/skeleton/app/HelloWorld.java
@@ -4,19 +4,39 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL30;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class HelloWorld implements ApplicationListener {
     private SpriteBatch batch;
     private BitmapFont font;
+    private TiledMap map;
+    private TiledMapTileLayer boardLayer, playerLayer, holeLayer, flagLayer;
+    private OrthogonalTiledMapRenderer myRenderer;
+    private OrthographicCamera myCam;
+
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         font = new BitmapFont();
+        map = new TmxMapLoader().load("assets/exampleMap.tmx");
+        boardLayer= (TiledMapTileLayer) map.getLayers().get("Board");
+        playerLayer= (TiledMapTileLayer) map.getLayers().get("Player");
+        holeLayer= (TiledMapTileLayer) map.getLayers().get("Hole");
+        flagLayer= (TiledMapTileLayer) map.getLayers().get("Flag");
         font.setColor(Color.RED);
-
+        myCam = new OrthographicCamera();
+        myCam.setToOrtho(false,5,5);
+        myCam.position.set(2.5F, 5.0F, 0.0F);
+        myCam.update();
+        myRenderer= new OrthogonalTiledMapRenderer(map,1F/300F);
+        myRenderer.setView(myCam);
     }
 
     @Override
@@ -30,9 +50,7 @@ public class HelloWorld implements ApplicationListener {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
 
-        batch.begin();
-        font.draw(batch, "Hello World", 200, 200);
-        batch.end();
+        myRenderer.render();
     }
 
     @Override
