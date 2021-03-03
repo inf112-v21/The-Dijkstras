c39e72d4541196622ebfc029c56fffc0e725adb6
Aleksander Glowacki <alexglo@outlook.com>
2021-03-03 14:05:04 +0100

    Changed Lwjgl3Application in Main to run RoboRally class
    - Also resized the app window

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -2,13 +2,14 @@ package inf112.skeleton.app;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import inf112.skeleton.RoboRally;
 
 public class Main {
     public static void main(String[] args) {
         Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
         cfg.setTitle("Robo Rally");
-        cfg.setWindowedMode(500, 500);
+        cfg.setWindowedMode(750, 750);
 
-        new Lwjgl3Application(new Board(), cfg);
+        new Lwjgl3Application(new RoboRally(), cfg);
     }
 }
