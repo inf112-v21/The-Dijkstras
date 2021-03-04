cd101e0ddfdcdc96af367300a97599b42237dafd
Aleksander Glowacki <alexglo@outlook.com>
2021-03-04 16:21:40 +0100

    Created a manual test for GUI.

---
+++ src/test/java/inf112/skeleton/app/AppManualTest.java
@@ -0,0 +1,47 @@
+package inf112.skeleton.app;
+import org.junit.Test;
+import org.junit.*;
+import java.util.Scanner; 
+
+
+import static org.hamcrest.CoreMatchers.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Manual test for GameApp.
+ *
+ * TODO: More Tests
+ * TODO: Run Screens Simultanous
+ */
+public class AppManualTest {
+
+    @Test
+    public void screenPopUp(){
+        String question = "Does Screen appear?";
+        String answer = askQuestion(question);
+        assertThat(answer, is("y"));
+
+    }
+
+
+    @Test
+    public void playerIsMovable(){
+        String question = "Is Player moveable with arrow keys";
+        String answer = askQuestion(question);
+        assertThat(answer, is("y"));
+    }
+
+
+    private static String askQuestion(String question){
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Do you see you screen y/n");
+        String answer = myScanner.nextLine();
+        if (answer.equals("y") || answer.equals("n")){
+            return answer;}
+        else{
+            System.out.println("Please answer with 'y' or 'n'");
+            return askQuestion(question);
+        }
+    }
+}
