ee2304c310185dcc8ecd8a48dc21fb79796a9c3f
Aleksander Glowacki <alexglo@outlook.com>
2021-03-04 16:22:42 +0100

    Made GameScreen now functional

--- src/main/java/inf112/skeleton/screens/GameScreen.java
+++ src/main/java/inf112/skeleton/screens/GameScreen.java
@@ -1,56 +1,53 @@
 package inf112.skeleton.screens;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputAdapter;
-import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.*;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.math.Vector2;
-import inf112.skeleton.RoboRally;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import inf112.skeleton.app.*;
 
 public class GameScreen extends ScreenAdapter {
-    RoboRally game;
+    private final GameInit gameInit;
+    private final Board board;
+    private Stage stage;
+    private Viewport viewport;
+    private InputMultiplexer inputMultiplexer;
+    private GameActor gameActor;
 
-    float circleX = 300;
-    float circleY = 150;
-    float circleRadius = 50;
-
-    float xSpeed = 4;
-    float ySpeed = 3;
-
-    public GameScreen(RoboRally game) {
-        this.game = game;
+    public GameScreen(GameInit gameInit, Board board, InputMultiplexer inputMultiplexer) {
+        this.gameInit = gameInit;
+        viewport = new ScreenViewport();
+        stage = new Stage(viewport);
+        this.board = board;
+        this.inputMultiplexer = inputMultiplexer;
+        inputMultiplexer.addProcessor(stage);
     }
 
     @Override
     public void show() {
-        Gdx.input.setInputProcessor(new InputAdapter() {
-            @Override
-            public boolean touchDown(int x, int y, int pointer, int button) {
-                int renderY = Gdx.graphics.getHeight() - y;
-                if (Vector2.dst(circleX, circleY, x, renderY) < circleRadius) {
-                    game.setScreen(new EndScreen(game));
-                }
-                return true;
-            }
-        });
+        float unitScale = 1/300f;
+        Skin skin = new Skin(Gdx.files.internal("assets/menuElements/roboRally.json"));
+
+        gameActor = new GameActor(gameInit, 0.5F);
+
+        Table cardsTable = new Table();
+        Label label = new Label("test123", skin);
+        cardsTable.add(label).center();
+
     }
 
     @Override
     public void render(float delta) {
-        circleX += xSpeed;
-        circleY += ySpeed;
-
-        if (circleX < 0 || circleX > Gdx.graphics.getWidth()) {
-            xSpeed *= -1;
-        }
-
-        if (circleY < 0 || circleY > Gdx.graphics.getHeight()) {
-            ySpeed *= -1;
-        }
 
         Gdx.gl.glClearColor(0, 0, .25f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
+        stage.addActor(gameActor);
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
 
     }
 
