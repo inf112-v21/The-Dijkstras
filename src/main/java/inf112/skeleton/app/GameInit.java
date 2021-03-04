7e4f522acc4778f15f591c46d0be5b3cd742c02f
Aleksander Glowacki <alexglo@outlook.com>
2021-03-04 16:24:03 +0100

    Added a helper class to initialize loading of a map

---
+++ src/main/java/inf112/skeleton/app/GameInit.java
@@ -0,0 +1,15 @@
+package inf112.skeleton.app;
+
+public class GameInit {
+
+    private final MapBuilder mapBuilder;
+
+    public GameInit (String map_filename, int playerLimit) {
+        mapBuilder = new MapBuilder(map_filename);
+
+
+    }
+    public MapBuilder getMapBuilder() {
+        return this.mapBuilder;
+    }
+}
