1fd2492539acc0b32778511165596ecda9cfa3b6
Erlend <erlend.h93@gmail.com>
2021-03-05 14:11:59 +0100

    hotfix Robot.java

--- src/main/java/inf112/skeleton/Game/Robot.java
+++ src/main/java/inf112/skeleton/Game/Robot.java
@@ -3,7 +3,6 @@ import inf112.skeleton.grid.Location;
 
     /**
      * Robot Class
-     * - Has 3 lives. Game over if 0
      * - Has 8 health. Loss life if 0
      * - StartPosition = (0,0)
      */
@@ -18,7 +17,6 @@ import inf112.skeleton.grid.Location;
             this.health = 8;
             this.position= new Location(0,0, 0);
             // this.direction = new Directions(0, -1); // North == (0, -1)?
-            //TODO se
         }
 
         public int getHealth() {
