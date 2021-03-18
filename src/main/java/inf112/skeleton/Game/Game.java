eb7a32bafdf340db03e4355b6de2bbbb383715b8
Trym Osland <trymguy@gmail.com>
2021-03-18 13:32:02 +0100

    added flagcheck in Game

--- src/main/java/inf112/skeleton/Game/Game.java
+++ src/main/java/inf112/skeleton/Game/Game.java
@@ -1,19 +1,29 @@
 package inf112.skeleton.Game;
 
 import inf112.skeleton.grid.*;
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
-    private GameBoard gameboard;
+    private GameBoard<Object> gameboard;
     private final List<Flag> flags;
-    private final HashSet<Player> players;
+    private final List<Player> players;
 
-    public Game(GameBoard gameboard, List<Flag> flags, HashSet<Player> players) {
+    public Game(GameBoard gameboard, List<Flag> flags, List<Player> players) {
         this.gameboard = gameboard;
         this.flags = flags;
         this.players = players;
     }
 
 
+
+    public void flagCheck() {
+        for (Player player : players) {
+            for (Flag flag : flags) {
+                if (gameboard.sameXYLocation(player, flag)) {
+                    player.checkFlagIndex(flag);
+                }
+            }
+        }
+    }
 }
