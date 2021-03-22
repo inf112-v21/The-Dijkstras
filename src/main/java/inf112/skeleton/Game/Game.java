e8bb013f29f56479b0a365b31b622f894ac8ecd5
Trym Osland <trymguy@gmail.com>
2021-03-22 13:25:38 +0100

    Made changed in Game and Barricade, tests not implemented

--- src/main/java/inf112/skeleton/Game/Game.java
+++ src/main/java/inf112/skeleton/Game/Game.java
@@ -2,6 +2,7 @@ package inf112.skeleton.Game;
 
 import inf112.skeleton.grid.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
@@ -16,6 +17,17 @@ public class Game {
     }
 
     public GameBoard getGameBoard() { return gameboard; }
+
+    public boolean validMove(Location loc, Directions dir) {
+        Location loc2 = loc.move(dir);
+        List<Object> XYObjects = gameboard.getXYObjects(loc2);
+        for (Object obj : XYObjects) {
+            if (obj instanceof Barricade) {
+                if (((Barricade) obj).isFacing(dir.rotate(2))) {return false;}
+            }
+        }
+        return true;
+    }
 
     public void wall(Location loc, Directions dir) {
         if (gameboard.get(loc) instanceof Barricade) {
