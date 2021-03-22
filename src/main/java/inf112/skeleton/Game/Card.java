7c3955baa0a9f85ebea17f08a49ac62c1b4fb36b
anas <anasalmelhem17@gmail.com>
2021-03-22 23:54:15 +0100

    Added toString method to Card class

--- src/main/java/inf112/skeleton/Game/Card.java
+++ src/main/java/inf112/skeleton/Game/Card.java
@@ -6,6 +6,9 @@ public class Card {
     public Card(CardType type, int priorityNr) {
         this.priorityNr= priorityNr;
         this.type= type;
-
+    }
+    @Override
+    public String toString(){
+        return type.name();
     }
 }
