d83c2e3d858599c7a387e29dcfe5f6f5a45e361b
Trym Osland <trymguy@gmail.com>
2021-03-24 12:39:17 +0100

    Implemented basic functionality of Game

--- src/main/java/inf112/skeleton/Game/Game.java
+++ src/main/java/inf112/skeleton/Game/Game.java
@@ -5,53 +5,34 @@ import java.util.List;
 
 public class Game {
     private boolean gameActive;
-    private GameBoard gameboard;
-    private final List<Flag> flags;
-    private final List<Player> players;
+    private RoundHandler rh;
 
-    public Game(GameBoard gameboard, List<Flag> flags, List<Player> players) {
-        this.gameboard = gameboard;
-        this.flags = flags;
-        this.players = players;
+    public Game(boolean gameActive, RoundHandler rh) {
+        this.gameActive = gameActive;
+        this.rh = rh;
     }
 
-    public GameBoard getGameBoard() { return gameboard; }
+    public void startGame() {
 
-    public boolean validMove(Location loc, Directions dir) {
-        Location loc2 = loc.move(dir);
-        List<ITileObject> XYObjects = gameboard.getXYObjects(loc2);
-        for (Object obj : XYObjects) {
-            if (obj instanceof Barricade) {
-                if (((Barricade) obj).isFacing(dir.rotate(2))) {return false;}
+        while (gameActive) {
+            //Deal programCards
+            //Let players register cards
+            //Let players choose whether to continuew running or pwoer down
+            completeRegisters();
+            //Clean up end-of-turn effects
+            boolean gameOver = true;
+            if (gameOver) {
+                this.gameActive = false;
             }
         }
-        return true;
+        return;
     }
 
-    private void placeBarricade(Location loc, Directions dir) {
-        if (gameboard.get(loc) instanceof Barricade) {
-            Barricade b = (Barricade) gameboard.get(loc);
-            b.addBarricade(dir);
-            gameboard.set(loc, b);
-        } else {
-            gameboard.set(loc, new Barricade(loc, dir));
-        }
-    }
+    public void completeRegisters() {
+        //Reveal program cards
+        //Robots move
+        //Board elements move
+        //Lasers fire
 
-    public void wall(Location loc, Directions dir) {
-        placeBarricade(loc, dir);
-        Location loc2 = loc.move(dir);
-        Directions dir2 = dir.rotate(2);
-        placeBarricade(loc2, dir2);
-    }
-
-    public void flagCheck() {
-        for (Player player : players) {
-            for (Flag flag : flags) {
-                if (gameboard.sameXYLocation(player.getRobot(), flag)) {
-                    player.checkFlagIndex(flag);
-                }
-            }
-        }
     }
 }
