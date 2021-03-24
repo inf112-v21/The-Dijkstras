b38a55b69b9de61b5e68871d97c2ff161f09c0c9
anas <anasalmelhem17@gmail.com>
2021-03-24 11:59:43 +0100

    Added restCard list and updateCurrentCards method to Player class

--- src/main/java/inf112/skeleton/Game/Player.java
+++ src/main/java/inf112/skeleton/Game/Player.java
@@ -14,23 +14,29 @@ public class Player implements ITileObject {
     private Location robotSpawnPoint;
     private Robot myRobot;
     private int nextFlagIndex = 1;
-    private boolean powerDown= false;
+    private boolean powerDown = false;
     private List<Card> hand = new ArrayList<>();
     private HashMap<Integer, Card> chosenCards = new HashMap<>();
+    private List<Card> restCards = new ArrayList<>();
 
-    public Player(Location startPosition){
+    public Player(Location startPosition) {
         this.life = 3;
         this.robotSpawnPoint = startPosition;
         this.myRobot = new Robot();
     }
 
     // Get && set robot
-    public void setRobot(Robot myRobot){ this.myRobot= myRobot; }
+    public void setRobot(Robot myRobot) {
+        this.myRobot = myRobot;
+    }
 
-    public Robot getRobot(){ return myRobot; }
+    public Robot getRobot() {
+        return myRobot;
+    }
 
 
-    public void placeRobotAtSpawn(GameBoard gb){
+    // kanskje denne metoden børe flyttes til GameBoard klasse fordi GameBoard styrer med posisjoner
+    public void placeRobotAtSpawn(GameBoard gb) {
         gb.set(robotSpawnPoint, myRobot);
     }
 
@@ -40,7 +46,8 @@ public class Player implements ITileObject {
     }
 
     public void newCheckPoint(Location location) {
-        this.robotSpawnPoint = location;}
+        this.robotSpawnPoint = location;
+    }
 
 
 
@@ -48,12 +55,12 @@ public class Player implements ITileObject {
         return this.life;
     }
 
-    public void decreaseLife(){
+    public void decreaseLife() {
         this.life -= 1;
     }
 
-    public int getNumberOfDamages(){
-        return 9-myRobot.getHealth();
+    public int getNumberOfDamages() {
+        return 9 - myRobot.getHealth();
     }
 
 
@@ -75,12 +82,12 @@ public class Player implements ITileObject {
         return powerDown;
     }
 
-    public void announcePowerDown(){
-        powerDown= true;
+    public void announcePowerDown() {
+        powerDown = true;
     }
 
-    public void cancelPowerDown(){
-        powerDown= false;
+    public void cancelPowerDown() {
+        powerDown = false;
     }
 
 
@@ -91,7 +98,9 @@ public class Player implements ITileObject {
         hand.addAll(cards);
     }
 
-    public List<Card> getHand() { return hand; }
+    public List<Card> getHand() {
+        return hand;
+    }
 
     public HashMap<Integer, Card> getChosenCards() { return chosenCards;}
 
@@ -106,6 +115,7 @@ public class Player implements ITileObject {
         int health = getRobot().getHealth();
         return Math.min(5, health);
     }
+
     /**
      * This method should check two conditions, the number of current chosen cards
      * and if the player still has time to choose
@@ -124,35 +134,61 @@ public class Player implements ITileObject {
     /**
      * Move the robot out from a card, moving occurs on the GameBoard if the card is a moving card,
      * Otherwise change the robot direction
+     *
      * @param movingCard the programming card
-     * @param gb GameBoard object where the moving is occurring
+     * @param gb         GameBoard object where the moving is occurring
      */
     public void makeMove(Card movingCard, GameBoard gb) {
-        Directions currentDir= myRobot.getDirection();
-        switch (movingCard.type){
+        Directions currentDir = myRobot.getDirection();
+        switch (movingCard.type) {
             case MOVE1:
-                gb.moveRobot(currentDir,myRobot);
+                gb.moveRobot(currentDir, myRobot);
                 break;
             case MOVE2:
-                gb.moveRobot(currentDir,myRobot);
-                gb.moveRobot(currentDir,myRobot);
+                gb.moveRobot(currentDir, myRobot);
+                gb.moveRobot(currentDir, myRobot);
                 break;
             case MOVE3:
-                gb.moveRobot(currentDir,myRobot);
-                gb.moveRobot(currentDir,myRobot);
-                gb.moveRobot(currentDir,myRobot);
+                gb.moveRobot(currentDir, myRobot);
+                gb.moveRobot(currentDir, myRobot);
+                gb.moveRobot(currentDir, myRobot);
                 break;
-            case BACKUP:   gb.moveRobot(currentDir.rotate(2),myRobot);
+            case BACKUP:
+                gb.moveRobot(currentDir.rotate(2), myRobot);
                 break;
-            case ROTLEFT:  myRobot.rotate(-1);
+            case ROTLEFT:
+                myRobot.rotate(-1);
                 break;
-            case ROTRIGHT: myRobot.rotate(+1);
+            case ROTRIGHT:
+                myRobot.rotate(+1);
                 break;
-            case UTURN:    myRobot.rotate(2);
+            case UTURN:
+                myRobot.rotate(2);
                 break;
-            default: throw new IllegalArgumentException(movingCard+" is not a valid card");
+            default:
+                throw new IllegalArgumentException(movingCard + " is not a valid card");
 
         }
 
+    }
+
+    public void updateCurrentCards() {
+        if (myRobot.getHealth() >= 5) {
+            restCards.addAll(currentCards.values());
+            currentCards.clear();
+        }
+        else {
+
+            int freeCards = myRobot.getHealth();
+            while (freeCards > 0) {
+                restCards.add(currentCards.remove(freeCards));
+                freeCards--;
+
+            }
+        }
+    }
+
+    public List<Card> getRestCards() {
+        return restCards;
     }
 }
