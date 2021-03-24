73dead262e219ed1a598f12b091c3d73689b5367
anas <anasalmelhem17@gmail.com>
2021-03-24 11:56:04 +0100

    Changed players type to HashSet

--- src/main/java/inf112/skeleton/Game/Game.java
+++ src/main/java/inf112/skeleton/Game/Game.java
@@ -1,26 +1,19 @@
 package inf112.skeleton.Game;
 
 import inf112.skeleton.grid.*;
+import java.util.HashSet;
 import java.util.List;
 
 public class Game {
     private GameBoard gameboard;
     private final List<Flag> flags;
-    private final List<Player> players;
+    private final HashSet<Player> players;
 
-    public Game(GameBoard gameboard, List<Flag> flags, List<Player> players) {
+    public Game(GameBoard gameboard, List<Flag> flags, HashSet<Player> players) {
         this.gameboard = gameboard;
         this.flags = flags;
         this.players = players;
     }
 
-    public void flagCheck() {
-        for (Player player : players) {
-            for (Flag flag : flags) {
-                if (gameboard.sameXYLocation(player.getRobot(), flag)) {
-                    player.checkFlagIndex(flag);
-                }
-            }
-        }
-    }
+
 }
